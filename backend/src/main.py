246fe59bd04a6f991261952d8d91fd5f748f8ede
jakubpedryc <jakubpedryc@gmail.com>
2025-07-17 20:30:26 +0200

    SQLite support

---
+++ backend/src/main.py
@@ -0,0 +1,17 @@
+import asyncio
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from src.db.init_db import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()  
+    yield             
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def read_root():
+    return {"message": "SQLite is connected"}
